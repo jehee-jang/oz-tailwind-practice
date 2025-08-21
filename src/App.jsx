6ca8f7ee244cf9188d8f763d7b6ce4bae0852531
jehee-jang <jehee010@naver.com>
2025-08-21 11:39:17 +0900

    과제 1차 완료

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 
+
+
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <section className="flex flex-wrap justify-center gap-[20px] py-[20px] px-[40px]">
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
